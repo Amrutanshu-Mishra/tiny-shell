f7ffdaf9a4e7c75a52f9a4bb6cfb07bf4b30a991
Amrutanshu mishra <amrutanshumishra607@gmail.com>
2026-08-09 09:29:22 +0000

    day3: implemented a basic command parser and basic command execution

--- src/main.c
+++ src/main.c
@@ -2,18 +2,25 @@
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<errno.h>
 
 #define MAX_INPUT 1024
 
 char** parse_input(char *input_string){
-    char **ptr=(char *)malloc(2*sizeof(char*));
+    char **ptr=(char **)malloc(2*sizeof(*ptr));
+
+    //handling the case in which malloc could fail
+    if(ptr==NULL){
+        return NULL;
+    }
+
     int ptr_size=0;
     int ptr_capacity=2;
 
-
-    char *dup_inp_str=input_string;
     char d[]=" ";
-    char *portion=strtok(dup_inp_str, d);
+    char *portion=strtok(input_string, d);
     int idx=0;
 
 
@@ -21,7 +28,7 @@ char** parse_input(char *input_string){
         //using the memory allocation pattern where we double the memory on each realloc
         if(ptr_size+1==ptr_capacity){
             ptr_capacity*=2;
-            char **tmp=realloc(ptr, ptr_capacity*sizeof(char*));
+            char **tmp=realloc(ptr, ptr_capacity*sizeof(*ptr));
             if(tmp==NULL){
                 free(ptr);
                 return NULL;
@@ -29,11 +36,13 @@ char** parse_input(char *input_string){
             ptr=tmp;
         }
         ptr[idx]=portion;
+        // printf("%s\n", portion);
         idx++;
         ptr_size++;
 
         portion=strtok(NULL, d);
     }
+    // idx--;
     ptr[idx]=NULL;
     
 
@@ -49,11 +58,66 @@ int main(){
         fflush(stdout);
 
         if(fgets(input, MAX_INPUT, stdin) == NULL){
-            perror("fgets falied");
+            printf("\n");
+            break;
+        }
+
+        //removing the \n from the input
+        input[strcspn(input, "\n")]='\0';
+
+
+        //modifying this input into a new char* so that it can be passed to the function
+        char *mod_input=strdup(input);
+        
+        //handling the case in which strdup could fail
+        if(mod_input==NULL){
+            perror("strdup");
+            continue;
+        }
+        
+        //parsing the mod input into token
+        char **input_token_array=parse_input(mod_input);
+        
+        //handling the edge case where input_token_array is NULL
+        if(input_token_array==NULL){
+            free(mod_input);
             continue;
         }
 
-        printf("You typed %s", input);
+        //handling the edge case when the user gives no input
+        if(input_token_array[0]==NULL){
+            free(input_token_array);
+            free(mod_input);
+            continue;
+        }
+
+        //Now calling a child process
+        pid_t pid=fork();
+
+        if(pid<0){
+            perror("fork");
+        }
+        else if(pid==0){
+            //inside the child process
+            execvp(input_token_array[0], input_token_array);
+
+            //if the command in the shell is not found
+            if(errno==ENOENT){  
+                fprintf(stderr,"%s: command not found\n", input_token_array[0]);
+            }
+            else{
+                perror(input_token_array[0]);
+            }
+
+            exit(EXIT_FAILURE);
+        }
+        else{
+            //the parent has to wait for th e
+            wait(NULL);
+        }
+
+        free(input_token_array);
+        free(mod_input);
     }
     
     return 0;
